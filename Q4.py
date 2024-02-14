5ab71c652472b1a2f13133a0935cb242c77219a6
feeble-menace23 <160043588+feeble-menace23@users.noreply.github.com>
2024-02-14 21:51:32 +0530

    Create Q4.py

---
+++ Q4.py
@@ -0,0 +1,23 @@
+def unduplicator():
+    el = [];
+    n = int(input("enter the number of elements\n"));
+    print("enter all elements:");
+
+    for i in range(n):
+        a = input();
+        el.append(a);
+
+    for j in range(n):
+        check = el[j];
+        if(check != '#'):
+            for k in range(n):
+                if el.index(check) != k:
+                    if el[k] == check:
+                        el[k] = '*';
+
+    print("new list without duplicates:");
+
+    for l in range(n):
+        if(el[l] != '*'):
+            print(el[l], end=" ");
+unduplicator();
